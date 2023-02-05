54bdcf6382bf7a557cda72c6e8d416dcbfb8d5b1
Mantou1233 <rwkn100@gmail.com>
2023-02-05 18:12:01 +0800

    big update

--- src/main/java/me/crystal/helloworld/HelloWorldPlugin.java
+++ src/main/java/me/crystal/helloworld/HelloWorldPlugin.java
@@ -2,6 +2,7 @@ package me.crystal.helloworld;
 
 import com.earth2me.essentials.Essentials;
 import me.crystal.helloworld.commands.BalanceCommand;
+import me.crystal.helloworld.commands.TestCommand;
 import me.crystal.helloworld.listeners.ChestGuiListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -30,6 +31,9 @@ public class HelloWorldPlugin extends JavaPlugin {
         // Register the example command
         Objects.requireNonNull(this.getCommand("shop")).setExecutor(new ShopCommand());
         Objects.requireNonNull(this.getCommand("balance")).setExecutor(new BalanceCommand());
+        TestCommand testCommandInstance = new TestCommand();
+        Objects.requireNonNull(this.getCommand("test")).setExecutor(testCommandInstance);
+        Objects.requireNonNull(this.getCommand("test")).setTabCompleter(testCommandInstance);
         
         // Register the example listener
         this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
---
+++ src/main/java/me/crystal/helloworld/commands/TestCommand.java
@@ -0,0 +1,44 @@
+package me.crystal.helloworld.commands;
+
+import me.crystal.helloworld.HelloWorldPlugin;
+import me.crystal.helloworld.listeners.ChestGuiListener;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCommand implements TabExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)){
+            sender.sendMessage("console cannot use!");
+            return false;
+        }
+        Player player = (Player) sender;
+        if(args[0] == "write"){
+            sender.sendMessage("OK");
+            HelloWorldPlugin.getInstance().getConfig().set("itemstack-test", player.getInventory().getItemInMainHand());
+        }
+        else if(args[0] == "read"){
+            ItemStack item = HelloWorldPlugin.getInstance().getConfig().getItemStack("itemstack-test");
+            if(item == null) return false;
+            sender.sendMessage("OK");
+            player.getInventory().addItem(item);
+        }
+        return true;
+    }
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        List<String> argList = new ArrayList<>();
+        argList.add("read");
+        argList.add("write");
+        return argList;
+    }
+}
